32320a684e0fb5edfd1243cbfd90c99009a3ebcd
yourunb <bxyura@gmail.com>
2025-03-10 19:03:59 +0300

    feat: create class and constructor

--- task-1/task-1/Program.cs
+++ task-1/task-1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//----------------------------------------------------------- 1. Создание одномерного массива.
+class AClass1
+{
+    public int[] iArray; //объявлен массив чисел
+    public string[] strArray; //обяъявлен массив строк
+
+    public AClass1() //конструктор
+    {
+        iArray = new int[10];
+        strArray = new string[5];
+    }
+}
