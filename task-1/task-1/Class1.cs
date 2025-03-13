db993674f3bfc9e70a12bc7a18781a345b34c109
yourunb <bxyura@gmail.com>
2025-03-13 10:07:44 +0300

    feat: create separate class file

---
+++ task-1/task-1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//----------------------------------------------------------- 1. Создание одномерного массива.
+namespace task_1
+{
+    class AClass1
+    {
+        public int[] iArray; //объявлен массив чисел
+        public string[] strArray; //обяъявлен массив строк
+
+        public AClass1() //конструктор
+        {
+            iArray = new int[10];
+            strArray = new string[5];
+        }
+    }
+}
--- task-1/task-1/Program.cs
+++ task-1/task-1/Program.cs
@@ -1,22 +1,5 @@
-﻿//----------------------------------------------------------- 1. Создание одномерного массива.
-class AClass1
-{
-    public int[] iArray; //объявлен массив чисел
-    public string[] strArray; //обяъявлен массив строк
+﻿using task_1;
 
-    public AClass1() //конструктор
-    {
-        iArray = new int[10];
-        strArray = new string[5];
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        AClass1 a1 = new AClass1(); //создать объект типа AClass1
-        a1.iArray[0] = 100;
-        Console.WriteLine(a1.iArray[0]);
-    }
-}
+AClass1 a1 = new AClass1(); //создать объект типа AClass1
+a1.iArray[0] = 100;
+Console.WriteLine(a1.iArray[0]);
