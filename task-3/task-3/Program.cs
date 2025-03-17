8604f3638eb5291c8ab22c0fa0ab4984a5827c4d
yourunb <bxyura@gmail.com>
2025-03-17 12:03:41 +0300

    feat: show results from console

--- task-3/task-3/Program.cs
+++ task-3/task-3/Program.cs
@@ -6,4 +6,10 @@
         new int[,] { {5,6}, {4,6}, {7,8} },
         new int[,] { {0,0}, {0,0}, {0,0} }
     };
+
+    Console.WriteLine(imyJaggedArray1.Rank);
+    Console.WriteLine(imyJaggedArray1.Length);
+    Console.WriteLine("{0}", imyJaggedArray1[0][1, 0]);
 }
+
+Main();
