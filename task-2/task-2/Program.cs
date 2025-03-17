c646c42594b5f1414dac17f066b0408f6461f9a0
yourunb <bxyura@gmail.com>
2025-03-17 11:10:26 +0300

    feat: add writeline for Main

--- task-2/task-2/Program.cs
+++ task-2/task-2/Program.cs
@@ -2,6 +2,9 @@
 {
     int[,] imyArray1 = new int[6, 3];
     int[,] imyArray2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+    Console.WriteLine("Всего элементов = {0}  первая размерность = {1} вторая размерность = {2}", imyArray1.Length, imyArray1.GetLength(0), imyArray1.GetLength(1));
+    Console.WriteLine("Всего элементов = {0}  первая размерность = {1} вторая размерность = {2}", imyArray2.Length, imyArray2.GetLength(0), imyArray2.GetLength(1));
 }
 
 Main();
