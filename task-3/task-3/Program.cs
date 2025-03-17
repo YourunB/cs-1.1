70e56932a998a8f085c372447c8f9b8095aa3a44
yourunb <bxyura@gmail.com>
2025-03-17 13:17:07 +0300

    refactor: delete double Main for not ignore C#

--- task-3/task-3/Program.cs
+++ task-3/task-3/Program.cs
@@ -3,20 +3,15 @@
     Console.WriteLine(" [0] = {0} [1] = {1}", i[0], i[1]);
 }
 
-static void Main()
+int[][,] imyJaggedArray1 = new int[3][,]
 {
-    int[][,] imyJaggedArray1 = new int[3][,]
-    {
-        new int[,] { {1,2}, {3,4} },
-        new int[,] { {5,6}, {4,6}, {7,8} },
-        new int[,] { {0,0}, {0,0}, {0,0} }
-    };
+    new int[,] { {1,2}, {3,4} },
+    new int[,] { {5,6}, {4,6}, {7,8} },
+    new int[,] { {0,0}, {0,0}, {0,0} }
+};
 
-    Console.WriteLine(imyJaggedArray1.Rank); // размерность созданного массива
-    Console.WriteLine(imyJaggedArray1.Length); // число элементов в созданном массиве
-    Console.WriteLine("{0}", imyJaggedArray1[0][1, 0]); // вывод значения элемента [1,0] из первого элемента одномерного массива
+Console.WriteLine(imyJaggedArray1.Rank); // размерность созданного массива
+Console.WriteLine(imyJaggedArray1.Length); // число элементов в созданном массиве
+Console.WriteLine("{0}", imyJaggedArray1[0][1, 0]); // вывод значения элемента [1,0] из первого элемента одномерного массива
 
-    InArray(new int[] { 1, 4 });
-}
-
-Main();
+InArray(new int[] { 1, 4 });
