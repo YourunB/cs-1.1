d3c584a04ad1b78866d6a7b5800f9f7b0e0e59f1
yourunb <bxyura@gmail.com>
2025-03-17 11:56:53 +0300

    feat: create imyJaggedArray1 from Main

---
+++ task-3/task-3/Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main()
+{
+    int[][,] imyJaggedArray1 = new int[3][,]
+    {
+        new int[,] { { 1, 2 }, {3, 4 } },
+        new int[,] { { 5, 6 }, { 4, 6 }, { 7, 8 } },
+        new int[,] { { 0, 0 }, { 0, 0}, {0, 0} }
+    };
+}
