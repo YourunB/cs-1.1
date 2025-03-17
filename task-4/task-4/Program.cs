4db3964e1662684e4cc61c6a8dd3f330a150343d
yourunb <bxyura@gmail.com>
2025-03-17 13:12:00 +0300

    feat: complete task 4

--- task-4/task-4/Program.cs
+++ task-4/task-4/Program.cs
@@ -1,2 +1,10 @@
-﻿var а = new[] { 1, 10, 100, 1000 };
+﻿static void InArray<T>(T[] i) // так будет работать любой массив
+{
+    Console.WriteLine(i.Length);
+}
+
+var а = new[] { 1, 10, 100, 1000 };
 var c = new[] { "string1", null, "string2" };
+
+InArray(а);
+InArray(c);
