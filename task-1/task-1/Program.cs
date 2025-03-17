747b22914c77bb2c0a40b6ee57f1d1805bb7a5d6
yourunb <bxyura@gmail.com>
2025-03-17 10:47:38 +0300

    feat: complete task 1

--- task-1/task-1/Program.cs
+++ task-1/task-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using task_1;
 
-AClass1 a1 = new AClass1(); //создать объект типа AClass1
+AClass1 a1 = new AClass1(); // создать объект типа AClass1
 a1.iArray[0] = 100;
 Console.WriteLine(a1.iArray[0]);
+Console.WriteLine(a1.iArray.GetLength(0)); // количество элементов в массиве
