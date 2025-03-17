8a8f97f852a66f076a29ad66b0b862cd357f8b9b
yourunb <bxyura@gmail.com>
2025-03-17 11:49:17 +0300

    feat: complete task 2

--- task-2/task-2/Program.cs
+++ task-2/task-2/Program.cs
@@ -5,6 +5,8 @@
 
     Console.WriteLine("Всего элементов = {0}  первая размерность = {1} вторая размерность = {2}", imyArray1.Length, imyArray1.GetLength(0), imyArray1.GetLength(1));
     Console.WriteLine("Всего элементов = {0}  первая размерность = {1} вторая размерность = {2}", imyArray2.Length, imyArray2.GetLength(0), imyArray2.GetLength(1));
+
+    Console.WriteLine("Число размерностей массива imyArray2 равно = {0} ", imyArray2.Rank); // размерность массива
 }
 
 Main();
