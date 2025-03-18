5564267bd92d55104df489f7d252a5935d8df0a6
yourunb <bxyura@gmail.com>
2025-03-18 11:30:02 +0300

    feat: add arrs and LINQ response

---
+++ task-6/task-6/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+string[] words = { "aPPLE", "ORangE", "cHeRry" };
+
+var numQuery =
+    from num in numbers
+    where (num % 2) == 0
+    select num;
+
+var varWords =
+    from word in words
+    select new { Upper = word.ToUpper(), Lower = word.ToLower() };
