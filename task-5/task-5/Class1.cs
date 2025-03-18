da698d2509f13907400bbc708a3bd44592295f99
yourunb <bxyura@gmail.com>
2025-03-18 10:13:27 +0300

    feat: add m1 method

--- task-5/task-5/Class1.cs
+++ task-5/task-5/Class1.cs
@@ -8,5 +8,9 @@ namespace task_5
 {
     internal class Class1
     {
+        public string m1(int I)
+        {
+            return "Параметр I=  " + System.Convert.ToString(I);
+        }
     }
 }
