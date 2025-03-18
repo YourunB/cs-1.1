09c13923e097b517cb54f475ea8d99cfd307c1e6
yourunb <bxyura@gmail.com>
2025-03-18 11:32:45 +0300

    feat: add foreach for show LINQ results

--- task-6/task-6/Program.cs
+++ task-6/task-6/Program.cs
@@ -6,6 +6,10 @@ var numQuery =
     where (num % 2) == 0
     select num;
 
+foreach (int num in numQuery) { Console.Write("{0,1} ", num); } // вывод четных чисел
+
 var varWords =
     from word in words
     select new { Upper = word.ToUpper(), Lower = word.ToLower() };
+
+foreach (var ul in varWords) { Console.WriteLine("Заглавные буква: {0}, Строчные буквы: {1}", ul.Upper, ul.Lower); }  // вывод слов в верхнем и нижнем регистре
