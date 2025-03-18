7263bd5c0e6d87b1a25d54eed1609bcefd68b88c
yourunb <bxyura@gmail.com>
2025-03-18 10:19:58 +0300

    feat: complete task 5

--- task-5/task-5/Program.cs
+++ task-5/task-5/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using task_5;
+
+Class1 CL1 = new Class1();
+
+string S = CL1.m1(123);
+Console.WriteLine(S);
+
+int i = CL1.m2("123");
+Console.WriteLine("Параметр s = " + System.Convert.ToString(i));
